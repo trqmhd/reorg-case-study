af675701470d2aa5e4f4435bb482fbea0400587e
Tariq Mahmood <tariq3185@gmail.com>
2023-04-11 07:23:11 -0400

    add print statement

--- apps/payment-search-tool-api/insert_data.py
+++ apps/payment-search-tool-api/insert_data.py
@@ -13,7 +13,7 @@ load_dotenv(path.join(basedir, '.env'))
 connection_string = os.environ.get('LOCAL_SQLALCHEMY_DATABASE_URI')
 engine = create_engine(connection_string)
 
-def update_data():
+def insert_data():
     # Download the data
     # URL to the zip file
     url = "https://download.cms.gov/openpayments/PGYR20_P012023.ZIP"
@@ -26,6 +26,7 @@ def update_data():
     with zipfile.ZipFile('openPaymentData.zip', 'r') as zip_ref:
         zip_ref.extractall('openPaymentData')
     print ("Zip file extracted...")
+
 
     script_path = os.path.abspath(__file__)
 
@@ -45,15 +46,15 @@ def update_data():
             break
     csv_file_path = str(folder_path + '/openPaymentData/' + matching_file)
 
+
     # Define the chunk size (number of rows to read at a time)
     chunk_size = 10000
     table_name = "payment"
-    print ("Data is being Updated...")
+    print ("Data in being inserted...")
     # Loop through the CSV file in chunks
     for chunk in pd.read_csv(csv_file_path, chunksize=chunk_size, low_memory=False):
         # Filter the data to find records where a doctor accepted gifts costing more than $10
         filtered_chunk = chunk[chunk["Total_Amount_of_Payment_USDollars"] > 15000]
-        
         # filtered_chunk.replace([np.inf, -np.inf], np.nan, inplace=True)
         
         # define a lambda function to truncate column names
@@ -62,30 +63,17 @@ def update_data():
         # use rename method with the lambda function to truncate all column names
         filtered_chunk = filtered_chunk.rename(columns=truncate_colname)
 
-        filtered_chunk = filtered_chunk.astype(str)
+        cols_to_string = filtered_chunk.columns[filtered_chunk.columns != 'Record_ID']
+        filtered_chunk[cols_to_string] = filtered_chunk[cols_to_string].astype(str)
 
-        rows_to_update = filtered_chunk[(filtered_chunk.Change_Type == 'CHANGED') | (filtered_chunk.Change_Type == 'ADD')]
-        rows_to_insert = filtered_chunk[filtered_chunk.Change_Type == 'NEW']
+        # Store the filtered data in the MySQL table using SQLAlchemy's to_sql() method
+        filtered_chunk.to_sql(name=table_name, con=engine, if_exists="append", index=False)
 
-        # Update rows
-        with engine.connect() as connection:
-            if not rows_to_update.empty:
-                for index, row in rows_to_update.iterrows():
-                    # Generate the SET clause dynamically
-                    set_clause = ', '.join([f"{column} = :{column}" for column in row.index if column != 'Record_ID'])
-                    update_query = text(f"UPDATE {table_name} SET {set_clause} WHERE Record_ID=:record_id")
-
-                    # Generate the query parameters dynamically
-                    query_params = {f"{column}": row[column] for column in row.index if column != 'Record_ID'}
-                    query_params['record_id'] = row['Record_ID']
-                    connection.execute(update_query, query_params)
-
-        # Insert rows
-        if not rows_to_insert.empty:
-            rows_to_insert.to_sql(name=table_name, con=engine, if_exists='append')
+    with engine.connect() as connection:
+        connection.execute(text(f'ALTER TABLE {table_name} ADD PRIMARY KEY (`Record_ID`);'))
 
     # Close the SQLAlchemy engine
     engine.dispose()
 
 if __name__ == '__main__':
-    update_data()
+    insert_data()
